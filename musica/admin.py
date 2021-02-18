98292305e679d09c5645ea9fd89bf60ca1147a29
ssc-gerardo <61258657+ssc-gerardo@users.noreply.github.com>
2021-02-18 12:37:21 -0600

    fixing models

--- musica/admin.py
+++ musica/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Canciones, Album, Artista, Tag, Genero, \
     SubGenero, Instrumento, Banda
 # Register your models here.
-admin.site.register(Canciones)
+admin.site.register(Banda)
 admin.site.register(Album)
 admin.site.register(Artista)
-admin.site.register(Tag)
+admin.site.register(Canciones)
 admin.site.register(Genero)
 admin.site.register(SubGenero)
+admin.site.register(Tag)
 admin.site.register(Instrumento)
-admin.site.register(Banda)
--- musica/models.py
+++ musica/models.py
@@ -17,6 +17,8 @@ class SubGenero(models.Model):
 
 class Banda(models.Model):
     nombre = models.CharField(max_length=100, null=False, blank=False)
+    similares = models.ManyToManyField(
+        "self", blank=True, related_name="similares")
 
     def __str__(self):
         return self.nombre
@@ -41,22 +43,7 @@ class Artista(models.Model):
         return self.nombre
 
 
-class Canciones(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
-    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, null=True)
-    subgenero = models.ForeignKey(
-        SubGenero, on_delete=models.CASCADE, null=True)
-    nombre = models.CharField(max_length=100)
-    duracion = models.TimeField(blank=True)
-    fecha = models.TimeField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nombre
-
-
 class Instrumento(models.Model):
-    cancion = models.ForeignKey(
-        Canciones, related_name='instrumentos', on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
@@ -64,9 +51,22 @@ class Instrumento(models.Model):
 
 
 class Tag(models.Model):
-    cancion = models.ForeignKey(
-        Canciones, related_name='tags', on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Canciones(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, blank=True)
+    subgenero = models.ForeignKey(
+        SubGenero, on_delete=models.CASCADE, null=True)
+    nombre = models.CharField(max_length=100)
+    duracion = models.TimeField(blank=True)
+    fecha = models.TimeField(blank=True, null=True)
+    instrumentos = models.ManyToManyField(Instrumento)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.nombre
